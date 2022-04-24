e3ae99c8b9a614577d799ebf27228fd26a02baed
Ali Asghar Kerai <ak06857@st.habib.edu.pk>
2022-04-25 02:50:02 +0500

    Health class mein panga

--- src/health.cpp
+++ src/health.cpp
@@ -24,16 +24,19 @@ void Health::updateHealth(Obstacles *obstacle)
     {
         Mix_PlayChannel(-1, Drawing::gCrash, 0);
         health -= 25;
+        cout << "Square detected" << endl;
     }
     else if (typeid(*obstacle).name() == typeid(wall).name())
     {
         Mix_PlayChannel(-1, Drawing::gCrash, 0);
         health -= 50;
+        cout << "Wall detected" << endl;
     }
     else if (typeid(*obstacle).name() == typeid(bomb).name())
     {
         Mix_PlayChannel(-1, Drawing::gExplode, 0);
         health = 0;
+        cout << "Bomb detected" << endl;
     }
     else if (typeid(*obstacle).name() == typeid(healthbump).name())
     {
@@ -41,11 +44,13 @@ void Health::updateHealth(Obstacles *obstacle)
         if (health != 100)
         {
             health += 25;
+            cout << "Health obstacle" << endl;
         }
     }
     else
     {
         health -= 25;
+        cout << "Square detected 2.0" << endl;
     }
 }
 /**
